3edfa4b3e76e5f90b19482432ac542b3e1f36c6f
SagarKumar9350 <psagar9350@gmail.com>
2025-12-25 14:22:34 +0530

    lecture 5

---
+++ lec-5/Example.java
@@ -0,0 +1,7 @@
+
+public class Example {
+    public static void main(String[] args) {
+        float a = .7f;
+        System.out.println((double)a); // it prints 0.699999988079071 not exactly 0.7
+    }
+}
