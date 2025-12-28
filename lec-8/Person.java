6aa68dcec08f73ec27f695c9e3396428850b9866
SagarKumar9350 <psagar9350@gmail.com>
2025-12-28 23:56:55 +0530

    lecture 8

---
+++ lec-8/Person.java
@@ -0,0 +1,9 @@
+
+public class Person {
+    Person(){
+        System.out.println("this is person constructor");
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ lec-8/Student.java
@@ -0,0 +1,7 @@
+
+public class Student extends Person {
+    Student(){
+        super();//automatically called if not written
+        System.out.println("this is student constructor");
+    }
+}
