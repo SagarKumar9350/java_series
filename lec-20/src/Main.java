841ff29d178188eef5789c1cb69ab7d077803ca9
SagarKumar9350 <psagar9350@gmail.com>
2026-01-01 23:39:21 +0530

    lecture 20

---
+++ lec-20/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)  {
+        int x = 2;
+        System.out.println("Hello, World!");
+        int z = (++x/((x++ -1)));
+        System.out.println("value of z is : "+z);
+    }
+}
