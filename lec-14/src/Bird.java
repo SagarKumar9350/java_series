6149b9bc70b63bea45c7e44438a84ede141c52e2
SagarKumar9350 <psagar9350@gmail.com>
2026-01-02 16:23:32 +0530

    rule update

--- lec-14/src/Bird.java
+++ lec-14/src/Bird.java
@@ -2,7 +2,7 @@ public interface Bird {
 
     
     //private int value = 10; // error -> only public,static,final is permitted
-     void canFly(); //methods are by default "public static"
+     void canFly(); //methods are by default "public"
 
 
      //private method is allowed after java9 but implementation has to be given
